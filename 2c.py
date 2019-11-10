12ae750d4820006ac3abcd3cbef4885a8a7dbcd2
Pulkit Dixit <31041140+pulkitdixit@users.noreply.github.com>
2019-11-10 13:17:37 -0600

    Add files via upload

---
+++ 2c.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 13:09:11 2019
+
+@author: Pulkit Dixit
+"""
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+import time
+from disc_def_max import discriminator
+
+batch_size = 128
+
+transform_test = transforms.Compose([
+    transforms.CenterCrop(32),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+testset = torchvision.datasets.CIFAR10(root='./', train=False, download=False, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
+testloader = enumerate(testloader)
+
+def plot(samples):
+    fig = plt.figure(figsize=(10, 10))
+    gs = gridspec.GridSpec(10, 10)
+    gs.update(wspace=0.02, hspace=0.02)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample)
+    return fig
+
+model = torch.load('cifar10.model')
+model.cuda()
+model.eval()
+
+batch_idx, (X_batch, Y_batch) = testloader.__next__()
+X_batch = Variable(X_batch,requires_grad=True).cuda()
+Y_batch_alternate = (Y_batch + 1)%10
+Y_batch_alternate = Variable(Y_batch_alternate).cuda()
+Y_batch = Variable(Y_batch).cuda()
+
+X = X_batch.mean(dim=0)
+X = X.repeat(batch_size,1,1,1)
+
+Y = torch.arange(batch_size).type(torch.int64)
+Y = Variable(Y).cuda()
+
+lr = 0.1
+weight_decay = 0.001
+for i in range(200):
+    _, output = model(X, 8)
+
+    loss = -output[torch.arange(batch_size).type(torch.int64),torch.arange(batch_size).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(batch_size))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/max_features.png', bbox_inches='tight')
+plt.close(fig)
